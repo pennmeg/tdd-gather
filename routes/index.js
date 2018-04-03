f9a840f924bf2625082a4f35fb596a3faf148286
Megan Penn <penn.meg@gmail.com>
2018-04-03 17:35:12 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const Item = require('../models/item');
+
+router.get('/', async (req, res, next) => {
+  const items = await Item.find({});
+  res.render('index', {items});
+});
+
+router.get('/items/create', async (req, res, next) => {
+  res.render('create');
+});
+
+router.post('/items/create', async (req, res, next) => {
+  const {title, description, imageUrl} = req.body;
+  const newItem = new Item({title, description, imageUrl});
+  newItem.validateSync();
+  if (newItem.errors) {
+    res.status(400).render('create', {newItem: newItem});
+  } else {
+    await newItem.save();
+    res.redirect('/');
+  }
+
+});
+router.get('items/:itemId', async (req, res, next) => {
+  const item = await Item.findById(req.params.item);
+  res.render('item: ', {item});
+});
+
+module.exports = router;
